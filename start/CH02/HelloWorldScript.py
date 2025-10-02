584d5652549915515896057adebb77d39eda7076
Sylent87 <dahl.erik@student.greenriver.edu>
2025-10-02 16:44:03 -0700

    HelloWorldHW 10_02_25

---
+++ start/CH02/HelloWorldScript.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# A simple "Hello World" script in python
+# Created by Sylent on Oct 2
+
+# Ask user for their name
+user_name = input("What is your name? ")
+
+# Print Hello and their name
+print("Hello {0}. Today is going to be a great day!".format(user_name))
