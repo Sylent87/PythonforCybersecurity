d7733c13f95b54de0c6c8a75a0f476c23b76424e
Sylent87 <dahl.erik@student.greenriver.edu>
2025-10-02 14:15:01 -0700

    Started Hello World 1-2

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by Sylent on Oct 2
+
+print("Hello World")
--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created 
+# Created by Sylent on Oct 2
+
+# Ask user for their name
+user_name = input("What is your name?")
+
+# Print Hello and their name
+print("Hello {0}".format(user_name))
 
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
