125e23718e8ae76e9b1dfc36b0c9f50140bb8c18
Sylent87 <dahl.erik@student.greenriver.edu>
2025-10-09 12:52:58 -0700

    Changes to code

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
-# example workign with conditionals
-#By 
+# example working with conditionals
+#By Sylent On Oct 9
+
+# Ask the user if today is a good day?
+user_answer = str(input("Is today a good day? (y/n) "))
+
+if user_answer == "y":
+    print("Yes it is!")
+
+if user_answer == "n":
+    print("No it's not.")
+
+elif user_answer == "Y":
+    print("It's a beautiful day.")
+
+elif user_answer == "N":
+    print("It will get better.")
+
+elif user_answer == "Yes":
+    print("It sure is")
+
+elif user_answer == "No""
--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Sylent on Oct 9
+
+i = 1
+while i <= 6:
+    print (i)
+    i += 1
+ 
