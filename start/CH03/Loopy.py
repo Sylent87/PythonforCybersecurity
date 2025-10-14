cad917ed40a130defbd1077bae1a71b2e38593cd
Sylent87 <dahl.erik@student.greenriver.edu>
2025-10-14 11:56:20 -0700

    Changes

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By Sylent on Oct 9
+#By Sylent on Oct 12
 
-i = 1
-while i <= 6:
-    print (i)
-    i += 1
- 
+# Ask the user if today is a good day?
+user_answer = str(input("Is today a good day? (y/n) "))
+
+if user_answer == "y":
+    for x in range(10):
+        print("Yes it is!")
