ec7d9d74e9eb92e13b464ba043c42f26bfccd03e
Sylent87 <dahl.erik@student.greenriver.edu>
2025-11-04 12:35:35 -0800

    Dictionary password

---
+++ start/CH05/Untitled-1.py
@@ -0,0 +1,5 @@
+
+score = add_score(190)
+def add_score(current):
+    return current + 10
+Print(name, "your score is", score)
--- start/CH06/dictionaryAttack.py
+++ start/CH06/dictionaryAttack.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 # Script that performs a dictionary attack against known password hashes
 # Needs a dictionary file, suggested to use https://github.com/danielmiessler/SecLists/tree/master/Passwords/Common-Credentials
-# By 
+# By Sylent
+
+import crypt
+import os
+
+#function to test the password
+def test_password(algorithm_salt, hashed_password, password_guess):
+    #Use salt to hash guess
+    hashed_guess = crypt.crypt(password_guess, algorithm_salt)
+    #Compare the two
+    if hashed_guess == hashed_password
+        return True
+    return False
+
+#load dictionary files
+dir_path = os.path.dirname(os.path.realpath(__file__))
+f = open(dir.path + "/top10.txt", "r")
+contents = f.readlines()
+
+#Prompt user for algorithm and salt
+algorithm_salt + input ("What is the Algorithm and Salt from the Shadow file?")
+#Prompt user for salted hash
+hashed_password = input ("What is the full hashed password including algor and salt?")
+#Loop through password
+for passwords in contents:
+    password = password.strip()
+    result = test_password(algorithm_salt, hashed_password)
+    if result:
+        print("Found it: {0}".format(password))
+        break
