a3aee2a0699cbe9116d217ba40fff4b51e296ad6
Sylent87 <dahl.erik@student.greenriver.edu>
2025-11-06 12:37:54 -0800

    Added code

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
-# By 
+# By Sylent
+
+import os
+
+#prompt for file, and open
+log_file = input("Which file do you want to see?")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+f = open(dir_path + "/" + log_file, "r")
+
+#read file by line
+while True:
+    line = f.readline()
+    if not line:
+        break
+    #Look for 404, and print out 404
+    if "404" in line:
+        print(line.strip)
+
+#close file
+f.close()
+
