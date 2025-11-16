bd5b70ce5816a1a319d38bca28e866e9390e5f6b
Sylent87 <dahl.erik@student.greenriver.edu>
2025-11-16 15:49:45 -0800

    I dont get it

--- end/CH08/PeopleInSpace1.py
+++ end/CH08/PeopleInSpace1.py
@@ -1,10 +1,18 @@
 import requests
+import json
 
 url = "http://api.open-notify.org/astros.json"
 
-payload = {}
+payload={}
 headers = {}
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+#print(response.text)
+
+astronauts = response.json()
+
+#print(astronauts)
+#print (json.dumps(astronauts, indent=2))
+print ("There are {0} people in space right now." .format(astronauts["number"]))
+print ("The first astronaut is {0}, aboard the {1}.".format(astronauts["people"][1]["name"], astronauts["people"[1]["craft"]]))
